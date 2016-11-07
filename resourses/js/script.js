57bf9bd99cf2ece96b838f6795b28f9576a4159e
Marko Zelenbaba <zelenbaba.marko@gmail.com>
2016-11-08 00:46:04 +0100

    code style

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -3,42 +3,42 @@ $(document).ready(function () {
     $('.menu').on('click', function () {
         $('.menu').removeClass('active');
         $(this).addClass('active');
-        $('.menu p').css("color", "white");
-        $(this).children("p").css("color", "black");
+        $('.menu p').css('color', 'white');
+        $(this).children('p').css('color', 'black');
         if ($(this).hasClass('appointment')) {
             $('.appointmentWrapInner').show();
         }
     });
     // open-close arrow popup
-    $(".userMenu").click(function (e) {
+    $('.userMenu').click(function (e) {
         e.preventDefault();
-        $(".arrowPopUp").css("display", "flex");
-        $(".arrowPopUp").focus();
+        $('.arrowPopUp').css('display', 'flex');
+        $('.arrowPopUp').focus();
     });
-    $(".arrowPopUp").on('blur', function () {
+    $('.arrowPopUp').on('blur', function () {
         $(this).fadeOut();
     });
     //close voucher
-    $("#closeVoucher").click(function () {
-        $(".boxVoucher").fadeOut(0);
+    $('#closeVoucher').click(function () {
+        $('.boxVoucher').fadeOut(0);
     });
     // open small Menu NAV 
-    $(".showMenu").click(function () {
+    $('.showMenu').click(function () {
         if ($(window).width() < 800) {
-            $("nav .menu").css("display", "flex");
+            $('nav .menu').css('display', 'flex');
         }
     });
     //change small menu NAV
-    $(".menu").click(function () {
-        var clicked = $(this).children("p").text();
+    $('.menu').click(function () {
+        var clicked = $(this).children('p').text();
         console.log(clicked);
         if ($(window).width() < 800) {
-            $(".showMenuText").html(clicked);
-            $("nav .menu").css("display", "none");
+            $('.showMenuText').html(clicked);
+            $('nav .menu').css('display', 'none');
         }
     });
     // preventdefault for arrow popup
-    $(".arrowPopUp a").click(function (e) {
+    $('.arrowPopUp a').click(function (e) {
         e.preventDefault();
     })
 });
